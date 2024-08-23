4c924f5b55cacfc6ef39993bedb18182bfeadd2c
Bob <chatemmybrown@gmail.com>
2024-08-23 17:16:59 +0100

    fridaycomit

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-
-const header = document.getElementById(`header`)
-const navLinks = [...document.querySelectorAll(`.link`)]
-const menuBar = [...document.querySelectorAll(`.menu-bar`)]
-const overlay = document.getElementById(`overlay-container`)
-const closeX = document.getElementById(`close-modal-icon`)
-const menuList = [...document.querySelectorAll(`.list-container p`)]
-const contactForm = document.getElementById(`contactForm`)
-let storageBox = []
-
-
-
-//Hover Effect to the header links + Menu overlay display
-navLinks.forEach(link=>{
-    link.addEventListener(`mouseenter`, function() {
-            this.style.color = `#D49E72`
-            this.style.transition = `color 0.3s ease`
-    })
-    link.addEventListener(`mouseleave`, function() {
-            this.style.color = `#0E2239`
-        })
-})
+/*
 
 menuBar.forEach(menu=>{
     menu.addEventListener(`mouseenter`, function() {
@@ -48,66 +27,6 @@ function resetAll() {
     }
 }
 
-//click action on menu list displayed on overlay 
-menuList.forEach(listItem=>{
-    listItem.addEventListener(`click`, (event)=>{
-      let click = event.target
-      let clickAction = click.dataset.action
-      if (clickAction === `home`) {
-        window.location.href = "index.html"
-      }else if (clickAction === `blog`) {
-        window.location.href = "https://hrlifestylewithdidi.blogspot.com/"
-      }else if(clickAction === `about`){
-        window.location.href = "about_us.html"
-      }else if(clickAction ===`contact`){
-        window.location.href = "contact.html"
-      }else if(clickAction === `services`){
-        window.location.href = "services.html"
-      }
-        resetAll()
-    })
-
-    window.addEventListener("popstate", (event) => {
-      // Do whatever you want when user navigates back
-      window.history.back
-  })
-})
-
-contactForm.addEventListener(`submit`, collectData)
-function collectData(event) {
-    event.preventDefault()
-
-    emailjs.sendForm('OsteoBob', 'OsteoBob', this)
-        .then(function(response) {
-            alert('Message sent successfully!');
-        }, function(error) {
-            alert('Failed to send the message. Please try again.');
-        });
-
-    /* Collect the form data
-    const namE = document.getElementById("name").value
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const message = document.getElementById("message").value;
-
-    if (namE === ``|| email ===``|| phone ===`` || message===``) {
-        alert `please input a text`
-    }else{   
-        let object = {
-            clntName: namE,
-            clntEmail: email,
-            clntPhone: phone,
-            clntMessage: message,
-        }
-        storageBox.push(object)
-        localStorage.setItem(`data`, JSON.stringify(storageBox))
-        console.log(storageBox);
-        contactForm.reset()
-    }*/
-    
-}
-
-/*
 //HOVER
 const imageCont = [...document.querySelectorAll(`.img`)]
 const rightCont = document.getElementById(`right-content`)
@@ -126,5 +45,112 @@ imageCont.forEach(image =>{
           nextImage.style.transform = `scale(1)`
   })
 })
-*/
+*/ 
+
+
+
+const header = document.getElementById(`header`)
+const navLinks = [...document.querySelectorAll(`.link`)]
+const contactForm = document.getElementById('contact-form')
+const menuIcon = document.getElementById(`menu`)
+const menuList = [...document.querySelectorAll(`.list`)]
+
+//Hover Effect to the nav links + Menu Icon display
+navLinks.forEach(link=>{
+  link.addEventListener(`mouseenter`, function() {
+          this.style.color = `#D49E72`
+          this.style.transition = `color 0.3s ease`
+  })
+  link.addEventListener(`mouseleave`, function() {
+          this.style.color = `#0E2239`
+      })
+})
+
+
+
+
+//click action on menu icon
+menuIcon.addEventListener(`click`, displayModal)
+function displayModal() {
+    const modal = document.getElementById('menuModal');
+    let span = document.getElementsByClassName('close')[0];
   
+
+    modal.style.display = 'flex';
+  
+    // Close the modal when the user clicks the close button
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+  
+    // Close the modal when the user clicks outside the modal content
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+    
+}
+//click action on menu list inside menuModal 
+menuList.forEach(listItem=>{
+    listItem.addEventListener(`click`, (event)=>{
+      let click = event.target
+      let clickAction = click.dataset.action
+      if (clickAction === `home`) {
+        window.location.href = "index.html"
+      }else if (clickAction === `blog`) {
+        window.location.href = "https://hrlifestylewithdidi.blogspot.com/"
+      }else if(clickAction === `about`){
+        window.location.href = "about_us.html"
+      }else if(clickAction ===`contact`){
+        window.location.href = "contact.html"
+      }else if(clickAction === `services`){
+        window.location.href = "services.html"
+      }
+    })
+
+    window.addEventListener("popstate", (event) => {
+      // Do whatever you want when user navigates back
+      window.history.back
+  })
+})
+
+
+
+
+//contact form collection
+contactForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  emailjs.sendForm('OsteoBo', 'OsteoBob', this)
+      .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          showModal('Message sent successfully!');
+      }, function(error) {
+          console.log('FAILED...', error);
+          showModal('Failed to send the message. Please try again.');
+      });
+      contactForm.reset()
+      
+});
+
+function showModal(message) {
+  let modal = document.getElementById('messageModal');
+  let modalMessage = document.getElementById('modal-message');
+  let span = document.getElementsByClassName('close')[1];
+
+  modalMessage.textContent = message;
+  modal.style.display = 'block';
+
+  // Close the modal when the user clicks the close button
+  span.onclick = function() {
+      modal.style.display = 'none';
+  }
+
+  // Close the modal when the user clicks outside the modal content
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = 'none';
+      }
+  }
+}
